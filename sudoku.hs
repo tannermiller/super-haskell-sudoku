8f7cfb9371f20fd096cdf1d70981217e93287dbb
Tanner Miller <tannermiller@gmail.com>
2013-06-07 12:47:42 -0500

    Add start of solving pieces.
    Doesn't work yet :)

--- sudoku.hs
+++ sudoku.hs
@@ -1,6 +1,6 @@
-import Data.Array
-import Data.Char
-import System.IO
+import Data.Array (Array, assocs, array)
+import Data.Char (digitToInt)
+import System.IO (putStrLn, readFile)
 
 main :: IO ()
 main = do
@@ -18,4 +18,22 @@ locList = [(x,y) | x <- [0..8], y <- [0..8]]
 -- Load the puzzle from a String
 load :: String -> Array (Int, Int) Int
 load = puzzle . zip locList . map digitToInt . filter ('\n' /=)
+
+-- Get all the values in a row
+getRow :: Array (Int, Int) Int -> Int -> [Int]
+getRow p row = filter check (assocs p)
+  where
+    check ((x, _), _) = x == row
+
+-- Get all the values in a column
+--getCol :: Array (Int, Int) Int -> Int -> [Int]
+
+-- Get all the values in a box
+--getBox :: Array (Int, Int) Int -> Int -> [Int]
+
+--isValid :: Array (Int, Int) Int -> (Int, Int) -> Int -> Bool
+--isValid puzzle loc value = 
+
+--solve :: Array (Int, Int) Int -> Array (Int, Int) Int
+--solve = 
 
