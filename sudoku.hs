bd02e08d9b4cdcc3e679042c473f0777e85aa0e8
Tanner Miller <tannermiller@gmail.com>
2013-06-08 21:34:54 -0500

    Expand load and fix getRows
    Expand load to handle space values and put in several
    local funcs in where clauses.
    
    Make getRows actually work

--- sudoku.hs
+++ sudoku.hs
@@ -2,28 +2,37 @@ import Data.Array (Array, assocs, array)
 import Data.Char (digitToInt)
 import System.IO (putStrLn, readFile)
 
+-- TODO: add tests
 main :: IO ()
 main = do
     putStrLn "Hello, World"
-    puzzle <- readFile "test.dat"
-    puzzle <- return $ load puzzle
+    puz <- readFile "test.dat"
+    puz <- return $ load puz
+    rows <- return $ getRow puz 0
+    putStrLn $ show rows
     putStrLn "Done"
 
 -- Define a puzzle as a 9x9 array
 puzzle :: [((Int, Int), e)] -> Array (Int, Int) e
 puzzle = array ((0,0), (8,8))
 
+-- list of array locations
 locList = [(x,y) | x <- [0..8], y <- [0..8]]
 
 -- Load the puzzle from a String
 load :: String -> Array (Int, Int) Int
-load = puzzle . zip locList . map digitToInt . filter ('\n' /=)
+load = puzzle . zipLoc . mapInt . mapBlanks . filter ('\n' /=)
+  where
+    zipLoc = zip locList
+    mapInt = map digitToInt
+    mapBlanks = map (\x -> if x == ' ' then '0' else x)
 
 -- Get all the values in a row
 getRow :: Array (Int, Int) Int -> Int -> [Int]
-getRow p row = filter check (assocs p)
+getRow p row = map clean $ filter check $ assocs p
   where
     check ((x, _), _) = x == row
+    clean = fst . fst
 
 -- Get all the values in a column
 --getCol :: Array (Int, Int) Int -> Int -> [Int]
