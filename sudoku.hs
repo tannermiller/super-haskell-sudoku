c9c2aa55e12c43167c6b36ec06fd95c2a1a8d9d6
Tanner Miller <tannermiller@gmail.com>
2013-08-18 20:31:44 -0500

    Committing to have available for later review
    Does not compile! Just pushing to github to have available to look at
    later.

--- sudoku.hs
+++ sudoku.hs
@@ -8,6 +8,7 @@ main = do
     putStrLn "Hello, World"
     puz <- readFile "test.dat"
     puz <- return $ load puz
+    putStrLn $ show (getBoxCol (0,0) (getBoxRow puz (0,0)))
     putStrLn "Done"
 
 -- Define a puzzle as a 9x9 array
@@ -28,17 +29,28 @@ load = puzzle . zipLoc . mapInt . mapBlanks . filter ('\n' /=)
 -- Get all the values in a row
 getRow :: Array (Int,Int) Int -> Int -> [Int]
 getRow p row = map snd $ filter check $ assocs p
-  where
-    check ((x, _),_) = x == row
+  where check ((x, _),_) = x == row
 
 -- Get all the values in a column
 getCol :: Array (Int,Int) Int -> Int -> [Int]
 getCol p col = map snd $ filter check $ assocs p
-  where
-    check ((_, y),_) = y == col
+  where check ((_, y),_) = y == col
 
 -- Get all the values in a box
---getBox :: Array (Int, Int) Int -> Int -> [Int]
+--getBox :: Array (Int,Int) Int -> (Int,Int) -> [Int]
+--getBox p loc = 
+
+getBoxRow :: Array (Int,Int) Int -> (Int,Int) -> [[Int]]
+getBoxRow p loc = case mod x 3 of 0 -> [getRow p i | i<-[x,x+1,(x+2)]]
+                                  1 -> [getRow p i | i<-[(x-1),x,(x+1)]]
+                                  2 -> [getRow p i | i<-[(x-2),(x-1),x]]
+  where x = fst loc
+
+getBoxCol :: (Int,Int) -> [[Int]] -> [[Int]]
+getBoxCol loc rows = map (map snd) (map colFilter [zip [0..] row | row<-rows])
+  where
+    colFilter = let y = snd loc in
+                filter (\(i,x) -> 
 
 --isValid :: Array (Int, Int) Int -> (Int, Int) -> Int -> Bool
 --isValid puzzle loc value = 
