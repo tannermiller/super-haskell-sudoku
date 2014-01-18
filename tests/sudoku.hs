757acd746247fdeefe6fb506a90221162823db18
Tanner Miller <tannermiller@gmail.com>
2014-01-17 23:10:15 -0600

    Add test for load
    Add test for load function.
    
    Vertically align all build-depends in .cabal file.

--- tests/sudoku.hs
+++ tests/sudoku.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
+import Data.Array (array)
 import Sudoku
 import Test.Framework.Providers.HUnit
 import Test.Framework.TH
@@ -8,6 +9,11 @@ import Test.Framework
 import Test.HUnit hiding (test)
 
 case_locList = [(x,y) | x <- [0..8], y <- [0..8]] @=? locList
+
+case_load = array ((0,0),(8,8)) expected @=? load input
+  where
+    expected = zip [(x,y) | x <- [0..8], y <- [0..8]] (concat [[1..9] | _ <- [0..8]])
+    input = unlines $ take 9 $ repeat $ foldl1 (++) $ map show [1..9]
 
 main :: IO ()
 main = defaultMain [$testGroupGenerator]
